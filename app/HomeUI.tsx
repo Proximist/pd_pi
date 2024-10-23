a9595aca603dea3364a4d40d6c13e2c017204e07
Unicorn X <dudebrothers2@gmail.com>
2024-10-23 04:22:55 -0700

    Update HomeUI.tsx

--- app/HomeUI.tsx
+++ app/HomeUI.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { toggleUpdateText } from './utils';
 import './HomeUI.css';
+import './globals.css';
 
 interface HomeUIProps {
   user: any;
