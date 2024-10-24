e1455d7f060b61e831f2741306e7170d89d4604b
Unicorn X <dudebrothers2@gmail.com>
2024-10-23 22:35:18 -0700

    Update TaskUI.tsx

--- app/task/TaskUI.tsx
+++ app/task/TaskUI.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import './TaskUI.css';
+import './TaskUIGEN.css';
 import '../globals.css'
 
 interface TaskUIProps {
