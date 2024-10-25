741b0ad17710837cac300bac05aa7dfad8e8f605
Proximist <103359694+Proximist@users.noreply.github.com>
2024-10-25 18:13:20 +0530

    Update page.tsx

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -76,7 +76,7 @@ export default function Invite() {
             setButtonState('initial')
             setNotification('')
           }, 300)
-        }, 500000)
+        }, 5000)
       }).catch(err => {
         console.error('Failed to copy: ', err)
         setNotification('Failed to copy invite link. Please try again.')
