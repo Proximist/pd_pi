6479d8ea4e296cd283bd69ba8ad152fcc3473d03
Proximist <103359694+Proximist@users.noreply.github.com>
2024-11-01 14:28:24 +0530

    Update page.tsx

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -89,6 +89,7 @@ const Timer: React.FC = () => {
   const footerContainerClass = `footerContainer ${isDarkMode ? 'dark-mode' : ''}`;
   const footerLinkClass = `footerLink ${isDarkMode ? 'dark-mode' : ''}`;
   const activeFooterLinkClass = `footerLink activeFooterLink ${isDarkMode ? 'dark-mode' : ''}`;
+  const loaderClass = `timer-loader ${isDarkMode ? 'dark-mode' : ''}`;
 
   const renderContent = () => {
     if (error) {
@@ -96,7 +97,7 @@ const Timer: React.FC = () => {
     }
 
     if (!countdownEndDate) {
-      return <div className="loader"></div>;
+      return <div className={loaderClass}></div>;
     }
 
     return (
